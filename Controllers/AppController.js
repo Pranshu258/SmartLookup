b15dc0a38cbe91502945f04a4716a5426c1cf23d
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-10-01 18:50:37 +0530

    Topics List Now works

---
+++ Controllers/AppController.js
@@ -0,0 +1,80 @@
+populatePage = function (text) {
+    $.ajax({
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.textrazor.com",
+        "method": "POST",
+        "headers": {
+            "x-textrazor-key": "b043156ea6a956b7b7cee9c9fdbead629578a609caace754ad13f958",
+            "accept-encoding": "application/gzip",
+            "content-type": "application/x-www-form-urlencoded"
+        },
+        "data": {
+            "extractors": "topics",
+            "text": text
+        },
+        "error": function (xhr, ajaxOptions, thrownError) {
+            console.log("Error in Text Razor Call",thrownError);
+        },
+        "success": function (data) {
+            console.log(data);
+            document.getElementById('result').innerHTML = data.response.topics[0]["label"];
+            var coarseTopics = data.response.coarseTopics;
+            var topics = data.response.topics;
+            var topicsList = [];
+            var topicsWikiNamesList = [];
+            var count;
+            var wikiLink;
+            var wikiName;
+            try {
+                var responseLength = topics.length;
+                if (topics.length > 30) responseLength = 30;
+                for (count = 0; count < responseLength ; count++) {
+                    topicsList.push(topics[count]["label"]);
+                    wikiLink = topics[count]["wikiLink"];
+                    wikiName = wikiLink.split(":")[2]
+                    topicsWikiNamesList.push(wikiName)
+                    //console.log(wikiName, topics[count]["label"])
+                }
+                console.log("Successfully retrived Topics from Text Razor");
+
+                runAllJobs(topicsList, topicsWikiNamesList);
+            }
+            catch (err) {
+                console.log("Couldn't retrive Topics from Text Razor", err);
+            };
+        }
+    });
+    return;
+}
+
+
+runAllJobs = function (topicsList, topicsWikiNamesList) {
+
+    $('#topics-list').html('<h3 class="josefin">Identifying the context of discussion...</h3>');
+    genTopicsListCard(topicsList);
+
+    // $('#wikipedia').html('<h1 class="josefin"><b>Wikipedia Excerpts</b></h1><p>I went through all of wikipedia to fetch articles, that might be relevant to you</p>');
+    // genWikipediaPage(topicsList, topicsWikiNamesList);
+    //
+    // $('#images').html('');
+    // genImages(topicsList);
+    //
+    // $('#videos').html('');
+    // genVideos(topicsList);
+    // //$('#articles').html('');
+    //
+    // $('#news').html('');
+    // genNews(topicsList);
+    //
+    // $('#twitter').html('');
+    // genTwitter(topicsList);
+    //
+    // $('#web').html('');
+    // genWeb(topicsList);
+    //
+    // genArticles(topicsList);
+
+
+
+}
---
+++ Controllers/TopicsListCardController.js
@@ -0,0 +1,21 @@
+﻿var genTopicsListCard = function (topicsList) {
+
+    var count;
+    var rowCount;
+    var html = '<h1><b>The Context</b></h1><h3>I think that you are trying to write about...</h3>';
+    for (count = 0; count <= parseInt(topicsList.length/12); count++) {
+        html = html + '<div class="row">'
+        for (rowCount = 0; rowCount < 12; rowCount++) {
+            if (((count * 12) + rowCount) >= topicsList.length) break;
+            html = html +
+                   '<h4 class="col-xs-6" style="text-align:center">' +
+                        topicsList[(count*12)+rowCount] +
+                    '</h4>'
+            ;
+        }
+        html = html + '</div>'
+    }
+    html = html + '<h3>Head on to <a data-toggle="tab" href="#wikipedia"> wikipedia</a> tab to peek at short summary of all</h3>';
+    $('#topics-list').html(html);
+    console.log("Generated Topics Card");
+}
